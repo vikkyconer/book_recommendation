bbb012fb45d9b405fccc518efad0d14002a6b466
Gurpreet Singh <gurpreetsingh_12644@aitpune.edu.in>
2015-03-28 06:17:07 +0530

    made books.py program General by getting id and access_token from user

--- books.py
+++ books.py
@@ -1,18 +1,18 @@
-#Print name of books liked by id=741504455940261 (Shubham Pendharkar).
+#Print name of books liked by id. Id and access token are given by user.
 
 import requests # pip install requests
 import json	
 
-base_url = 'https://graph.facebook.com/636504769758172'
-ACCESS_TOKEN='CAACEdEose0cBAIkAki14SzZAElas0RShemivQ91wEsHA8nxbzrZBifHgOzECKqihNX4Ew6Mth4MZAAXsxaasdFpZBwEebhEhXdC6oU9VDqdZC6kkukykprkq2Ge99RIYjtDJJzlv05yZCqC79ftkqK3w9lP5NrA0ZANMqhuL10Ui9ZAX3rbH2XcNLsh7ZBuMKy6H7Ex9EftOV3QBvsHkT1Pb8Q1QrZB2lxOAcZD'
+base_url = raw_input("Enter the id :") 
+base_url = 'https://graph.facebook.com/' + base_url
+ACCESS_TOKEN = raw_input("Paste here the access token :")
 fields ='books{name,likes}'
-
 
 def get_books():
     """
         Returns the list of posts on my timeline
     """
-
+    
     parameters = {'access_token': ACCESS_TOKEN}
     r = requests.get('https://graph.facebook.com/741504455940261/books', params=parameters)
     result = json.loads(r.text)
@@ -20,7 +20,4 @@ def get_books():
 
 r=get_books()
 for book in r:
-	print book['name']	
-
-
-
+	print book['name']
