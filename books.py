6c4e58960937029f38ea6ce12e0f2a64e3de7c12
Gurpreet Singh <gurpreetsingh_12644@aitpune.edu.in>
2015-03-28 06:41:54 +0530

    made output verbose

--- books.py
+++ books.py
@@ -3,16 +3,16 @@
 import requests # pip install requests
 import json	
 
-base_url = raw_input("Enter the id :") 
+base_url = raw_input("Enter the id : ") 
 base_url = 'https://graph.facebook.com/' + base_url
-ACCESS_TOKEN = raw_input("Paste here the access token :")
+ACCESS_TOKEN = raw_input("Paste here the access token : ")
 fields ='books{name,likes}'
 
 def get_books():
     """
         Returns the list of posts on my timeline
     """
-    
+
     parameters = {'access_token': ACCESS_TOKEN}
     r = requests.get('https://graph.facebook.com/741504455940261/books', params=parameters)
     result = json.loads(r.text)
@@ -20,4 +20,4 @@ def get_books():
 
 r=get_books()
 for book in r:
-	print book['name']
+	print "Book:" + book['name'] + "is liked by " + book['likes'] + '\n'
