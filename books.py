e544be89ec3c43c1a21cafdfc7ad12c81db9b921
Vikas Chandra <vikkycorner@gmail.com>
2015-04-03 23:51:59 +0530

    solved the problem if user had not read any books

--- books.py
+++ books.py
@@ -4,9 +4,12 @@ import requests # pip install requests
 import json	
 
 base_url = raw_input("Enter the id : ") 
+#base_url = '636504769758172' #naresh id
+#base_url = '976238135727023'
 base_url = 'https://graph.facebook.com/v2.3/' + base_url + '?'
 ACCESS_TOKEN = raw_input("Paste here the access token : ")
-fields ='books{name,likes}'
+#ACCESS_TOKEN = 'CAACEdEose0cBAGqs1WOWOBp22B0kvWItZCwPfiKHFyaWA45NZCvtgL8cS3USqWC7CUT8spTbJDKgU2D7x5bVm7w33IYiTjqUjK56uJOA1hpjpouEEKRcj6DnxoF3CO52kv5K1ouq4lrH24QyBS7BE5LcSw6RiYBC6UxN39ZAcugReNfdFhmiWhqJAIY6egEF6aRalIfk3mdu8yWCJ1yk9MqsjhiMHYZD'
+fields ='name,books{name,likes}'
 
 def get_books():
     """
@@ -16,9 +19,17 @@ def get_books():
     url = base_url + 'fields=' + fields + '&access_token='+ACCESS_TOKEN	
     r = requests.get(url)
     result = json.loads(r.text)
-    books = result["books"]
-    return books['data']
+    print result['name']
+    try:
+	print result['books']
+	books = result["books"]
+	return books['data']
+    except:
+	print "Not read any books"
+    return 0
 
 r=get_books()
-for book in r:
+if r != 0:
+    for book in r:
 	print "Book:" + book['name'] #+ "is liked by " + book['likes'] + '\n'
+
