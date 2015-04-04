ba0ca76b27fa066d7f31f6f517bdd0a8aeb99513
Gurpreet Singh <gurpreetsingh_12644@aitpune.edu.in>
2015-04-04 14:28:08 +0530

    Showing number of likes along with book's name with fromated output

--- books.py
+++ books.py
@@ -3,35 +3,44 @@
 import requests # pip install requests
 import json	
 
+MAX_SPACE = 60
 #base_url = raw_input("Enter the id : ") 
 #base_url = '636504769758172' #naresh id
-base_url = '976238135727023'
-base_url = 'https://graph.facebook.com/v2.3/' + base_url + '?'
-#ACCESS_TOKEN = raw_input("Paste here the access token : ")
-ACCESS_TOKEN = 'CAACEdEose0cBAGqs1WOWOBp22B0kvWItZCwPfiKHFyaWA45NZCvtgL8cS3USqWC7CUT8spTbJDKgU2D7x5bVm7w33IYiTjqUjK56uJOA1hpjpouEEKRcj6DnxoF3CO52kv5K1ouq4lrH24QyBS7BE5LcSw6RiYBC6UxN39ZAcugReNfdFhmiWhqJAIY6egEF6aRalIfk3mdu8yWCJ1yk9MqsjhiMHYZD'
-fields = 'friends{books,name}'
+#base_url = '976238135727023'
+#base_url = '738301736264268' # GSB id 
+base_url = 'https://graph.facebook.com/v2.3/me?'
+ACCESS_TOKEN = raw_input("Paste here the access token : ")
+fields = 'friends{name,books{name,likes}}'
+
+def get_spaces(some_string):
+    length_of_string = len(some_string)
+    spaces = MAX_SPACE - length_of_string
+    return " " * spaces
+    
 def get_books():
     """
         Returns the list of posts on my timeline
     """
 
-    url = base_url + 'fields=' + fields + '&access_token='+ACCESS_TOKEN	
+    url = base_url + 'fields=' + fields + '&access_token=' + ACCESS_TOKEN
     r = requests.get(url)
     result = json.loads(r.text)
     friends = result['friends']
     friends_list = friends['data']
+    return friends_list
+
+def print_books(friends_list):
     for friend_books in friends_list:
-	print ""
-	print friend_books['name']
-	print ""
+        print '\n' + str(friend_books['name']) + '\n'
         try:
-	    books = friend_books["books"]
-	    book_data = books['data']
-	    for book in book_data:
-		print "Book:" + book['name'] #+ "is liked by " + book['likes'] + '\n'
+            #print "In Try"
+            books = friend_books["books"]["data"]
+            #print books
+            for book in books:
+                print '\t' + book['name'] + get_spaces(book['name']) + "likes =" + str(book['likes'])
+                
         except:
-	    print "Not read any books"
-    #next = friends['paging']        
+            print '\t' + "Not read any books"
+    #next = friends['paging']
 
-get_books()
-
+print_books(get_books())
